3f222ab1992817fc4bfc82c4236d1e055693e78a
Tyler J. Parker <tjparker@csis.svsu.edu>
2018-02-25 10:40:02 -0500

    tjparker: added qm_ques_create.php

--- qm_ques_create.php
+++ qm_ques_create.php
@@ -1,7 +1,7 @@
 
 <?php
 /* ---------------------------------------------------------------------------
- * filename    : qm_quiz_create.php
+ * filename    : qm_ques_create.php
  * author      : Tyler Parker, tjparker@svsu.edu
  * description : This php file will create a new question (table: qm_question)
  * id   [auto incremented]
@@ -18,26 +18,27 @@ if(!isset($_SESSION["fr_person_id"])){ // if "user" not set,
 	exit;
 }
 */
-require '../../database/database.php';
+//require '../../database/database.php';
+require '/home/gpcorser/public_html/database/database.php';
 if ( !empty($_POST)) { // if not first time through
 	// initialize user input validation variables
-  $idError = null;
+  //$idError = null;
 	$quiz_idError = null;
 	$ques_nameError = null;
 	$ques_textError = null;
 	
 	// initialize $_POST variables
-	$id = $_POST['id'];
-	$pid = $_POST['quiz_id'];
+	//$id = $_POST['id'];
+	$qid = $_POST['quiz_id'];
 	$qName = $_POST['ques_name'];
-	$description = $_POST['ques_text'];		
+	$qText = $_POST['ques_text'];		
 	
 	// validate user input
-	$valid = true;
-	if (empty($id)) {
-		$idError = 'Please enter ID of the question';
-		$valid = false;
-	}
+	//$valid = true;
+	//if (empty($id)) {
+	//	$idError = 'Please enter ID of the question';
+	//	$valid = false;
+	//}
 	if (empty($qid)) {
 		$quiz_idError = 'Please enter Quiz ID';
 		$valid = false;
@@ -54,15 +55,17 @@ if ( !empty($_POST)) { // if not first time through
 	if ($valid) {
 		$pdo = Database::connect();
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "INSERT INTO qm_questions (id, quiz_id, ques_name, ques_text) values(?, ?, ?, ?)";
+		$sql = "INSERT INTO qm_questions (quiz_id, ques_name, ques_text) values(?, ?, ?)";
 		$q = $pdo->prepare($sql);
-		$q->execute(array($id,$qid,$qName,$qText));
+		$q->execute(array($qid,$qName,$qText));
 		Database::disconnect();
-		header("Location: qm_ques_create.php");
+		header("Location: qm_ques_list.php");
 	}
 }
-include '../../database/header.php'; //html <head> section
+//include '../../database/header.php'; //html <head> section
+include '/home/gpcorser/public_html/database/header.php'; //html <head> section
 ?>
+<html>
 <body>
     <div class="container">
 		<div class="span10 offset1">
@@ -71,24 +74,13 @@ include '../../database/header.php'; //html <head> section
 				<h3>Add New Question</h3>
 			</div>
 	
-			<form class="form-horizontal" action="qm_quiz_create.php" method="post">
-			
-				<div class="control-group <?php echo !empty($idError)?'error':'';?>">
-					<label class="control-label">id</label>
-					<div class="controls">
-						<input name="id" type="text"  placeholder="ID" value="<?php echo !empty($id)?$id:'';?>">
-						<?php if (!empty($idError)): ?>
-							<span class="help-inline"><?php echo $idError;?></span>
-						<?php endif; ?>
-					</div>
-				</div>
-			  
+			<form class="form-horizontal" action="qm_ques_list.php" method="post">						  
 				<div class="control-group <?php echo !empty($ques_nameError)?'error':'';?>">
 					<label class="control-label">Quiz ID</label>
 					<div class="controls">
-						<input name="per_id" type="text" placeholder="Time" value="<?php echo !empty($time)?$time:'';?>">
+						<input name="quiz_id" type="text" placeholder="Quiz ID" value="<?php echo !empty($qid)?$qid:'';?>">
 						<?php if (!empty($ques_nameError)): ?>
-							<span class="help-inline"><?php echo $ques_nameError;?></span>
+							<span class="help-inline"><?php echo $quiz_idError;?></span>
 						<?php endif;?>
 					</div>
 				</div>
@@ -96,7 +88,7 @@ include '../../database/header.php'; //html <head> section
 				<div class="control-group <?php echo !empty($quiz_nameError)?'error':'';?>">
 					<label class="control-label">Question Name</label>
 					<div class="controls">
-						<input name="quiz_name" type="text" placeholder="Location" value="<?php echo !empty($location)?$location:'';?>">
+						<input name="ques_name" type="text" placeholder="Question Name" value="<?php echo !empty($qName)?$qName:'';?>">
 						<?php if (!empty($quiz_nameError)): ?>
 							<span class="help-inline"><?php echo $quiz_nameError;?></span>
 						<?php endif;?>
@@ -106,7 +98,7 @@ include '../../database/header.php'; //html <head> section
 				<div class="control-group <?php echo !empty($ques_textError)?'error':'';?>">
 					<label class="control-label">Question Text</label>
 					<div class="controls">
-						<input name="quiz_description" type="text" placeholder="Description" value="<?php echo !empty($description)?$description:'';?>">
+						<input name="ques_text" type="text" placeholder="Ask Your Question" value="<?php echo !empty($qText)?$qText:'';?>">
 						<?php if (!empty($ques_textError)): ?>
 							<span class="help-inline"><?php echo $ques_textError;?></span>
 						<?php endif;?>
@@ -115,7 +107,7 @@ include '../../database/header.php'; //html <head> section
 				
 				<div class="form-actions">
 					<button type="submit" class="btn btn-success">Create</button>
-					<a class="btn" href="qm_quiz.php">Back</a>
+					<a class="btn" href="qm_ques_list.php">Back</a>
 				</div>
 				
 			</form>
