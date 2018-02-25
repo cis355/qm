f15ac6bcc6166c022ce4be4b4e053f9919350383
Brandon Gage <bgage@csis.svsu.edu>
2018-02-24 21:11:02 -0500

    bgage: updated qm_qa_list.php

--- qm_qa_list.php
+++ qm_qa_list.php
@@ -2,7 +2,7 @@
 /* ---------------------------------------------------------------------------
  * filename    : qm_qa_list.php
  * author      : Gage, Brandon bgage@svsu.edu
- * description :
+ * description : Lists the attempts of a given quiz for a single person
  * ---------------------------------------------------------------------------
  */
  /*
@@ -20,6 +20,15 @@ include '../../database/header.php'; // html <head> section
 <body style="background-color: lightblue !important";>
     <div class="container">
 		<div class="row">
+      <?php
+      require '../../database/database.php';
+      $pdo = Database::connect();
+      $sql = 'SELECT quiz_name, fname, lname FROM qm_attempts WHERE qm_attempts.quiz_id = ? Join qm_quizzes on qm_attempts.quiz_id = qm_quizzes.quiz_id Join qm_persons on qm_quizzes.per_id = qm_persons.id'
+      $q = $pdo->prepare($sql);
+      $q->execute(array($id));
+      $data = $q->fetch(PDO::FETCH_ASSOC);
+      echo 'Quiz Attemts on Quiz:' . $data['quiz_name'] . 'made by: ' . $data['lname'] . ', ' . $data['fname'];
+      ?>
 			<h3>Person</h3>
 		</div>
 		<div class="row">
@@ -41,15 +50,17 @@ include '../../database/header.php'; // html <head> section
 				</thead>
 				<tbody>
 					<?php
-						include '../../database/database.php';
+						require '../../database/database.php';
 						$pdo = Database::connect();
-						$sql = 'SELECT * FROM qm_persons';
+						$sql = 'SELECT qa_score, qa_start_date, qa_start_time, qa_end_date, qa_end_time FROM qm_attempts WHERE quiz_id = ?';
 						foreach ($pdo->query($sql) as $row) {
 							echo '<tr>';
-							echo '<td>'. trim($row['lname']) . '</td>';
-							echo '<td>'. trim($row['fname']) . '</td>';
-							echo '<td>'. trim($row['email']) . '</td>';
-							echo '<td>'. 'read update delete' . '<a href=qm_qa_read.php?quiz_id></a>' . '</td>';
+							echo '<td>'. trim($row['qa_score']) . '</td>';
+							echo '<td>'. trim($row['qa_start_date']) . '</td>';
+							echo '<td>'. trim($row['qa_start_time']) . '</td>';
+              echo '<td>'. trim($row['qa_end_date']) . '</td>';
+              echo '<td>'. trim($row['qa_end_time']) . '</td>';
+							echo '<td>'. '<a href=qm_qa_read.php?quiz_id>Read</a>'. '<a href="qm_qa_delete?quiz_id" class="btn btn_delete">Delete</a>' . '</td>';
 						}
 						Database::disconnect();
 					?>
