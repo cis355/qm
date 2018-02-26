ede8ed4a9fb64bfccec9899b0691d732c0b15be2
Shrijesh Siwakoti <ssiwakot@csis.svsu.edu>
2018-02-25 19:01:51 -0500

    ssiwakot: changed attribute names

--- qm_option_read.php
+++ qm_option_read.php
@@ -42,13 +42,13 @@
 					<label class="control-label">Option Text</label>
 					<div class="controls ">
 						<label class="checkbox">
-							<?php echo $data['option_text'];?> 
+							<?php echo $data['opt_text'];?> 
 						</label>
 					</div>
 					<label class="control-label">Option is Correct?</label>
 					<div class="controls ">
 						<label class="checkbox">
-							<?php echo $data['option_isCorrect'];?> 
+							<?php echo $data['opt_isCorrect'];?> 
 						</label>
 					</div>
 					<div class="form-actions">
