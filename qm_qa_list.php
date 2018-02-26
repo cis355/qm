f483a33c46cd2def71bcdc16efdf3baffe26e067
Brandon Gage <bgage@csis.svsu.edu>
2018-02-26 11:17:50 -0500

    bgage: made update to code in qm_qa_list.php

--- qm_qa_list.php
+++ qm_qa_list.php
@@ -23,7 +23,7 @@ include '../../database/header.php'; // html <head> section
       <?php
       require '../../database/database.php';
       $pdo = Database::connect();
-      $sql = 'SELECT quiz_name, fname, lname FROM qm_attempts WHERE qm_attempts.quiz_id = ? Join qm_quizzes on qm_attempts.quiz_id = qm_quizzes.quiz_id Join qm_persons on qm_quizzes.per_id = qm_persons.id';
+      $sql = 'SELECT quiz_name, fname, lname FROM qm_attempts WHERE qm_attempts.quiz_id ='.  $_GET("quiz_id") . 'Join qm_quizzes on qm_attempts.quiz_id = qm_quizzes.quiz_id Join qm_persons on qm_quizzes.per_id = qm_persons.id';
       $q = $pdo->prepare($sql);
       $q->execute(array($id));
       $data = $q->fetch(PDO::FETCH_ASSOC);
@@ -51,7 +51,7 @@ include '../../database/header.php'; // html <head> section
 					<?php
 						require '../../database/database.php';
 						$pdo = Database::connect();
-						$sql = 'SELECT qa_score, qa_start_date, qa_start_time, qa_end_date, qa_end_time FROM qm_attempts WHERE quiz_id = ?';
+						$sql = 'SELECT qa_score, qa_start_date, qa_start_time, qa_end_date, qa_end_time FROM qm_attempts WHERE quiz_id = ' . $_GET("quiz_id");
 						foreach ($pdo->query($sql) as $row) {
 							echo '<tr>';
 							echo '<td>'. trim($row['qa_score']) . '</td>';
@@ -59,7 +59,7 @@ include '../../database/header.php'; // html <head> section
 							echo '<td>'. trim($row['qa_start_time']) . '</td>';
               echo '<td>'. trim($row['qa_end_date']) . '</td>';
               echo '<td>'. trim($row['qa_end_time']) . '</td>';
-							echo '<td>'. '<a href=qm_qa_read.php?quiz_id>Read</a>'. '<a href="qm_qa_delete?quiz_id" class="btn btn_delete">Delete</a>' . '</td>';
+							echo '<td>'. '<a href="qm_qa_read.php?quiz_id=' . $_GET("quiz_id").'">Read</a>'. '<a href="qm_qa_delete?quiz_id='. $_GET("quiz_id").'" class="btn btn_delete">Delete</a>' . '</td>';
 						}
 						Database::disconnect();
 					?>
