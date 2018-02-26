bb2123e19c8adbb156e5c6ad954e5024db80b67f
George P. Corser <gpcorser@csis.svsu.edu>
2018-02-26 10:19:31 -0500

    gpcorser: option changes

--- qm_option_create.php
+++ qm_option_create.php
@@ -5,36 +5,32 @@
  * description : This program adds/inserts a new option (table: qm_options)
  * ---------------------------------------------------------------------------
  */
-
-
-require '/home/gpcorser/public_html/database/database.php';
-
 include '/home/gpcorser/public_html/database/header.php'; // html <head> section
 include '/home/gpcorser/public_html/database/database.php'; // gpcorser
-
 
 if ( !empty($_POST)) { // if not first time through
 
 	// initialize user input validation variables
 	// $ques_idError = null; // gpcorser: never let user choose id
+	//$ques_idError = null;
 	$opt_textError = null;
 	$opt_isCorrectError = null;
 
 	// initialize $_POST variables
 	// $ques_id = $_POST['ques_id']; // gpcorser: never let user choose id
+	$ques_id = $_POST['ques_id'];
 	$opt_text = $_POST['opt_text'];
 	$opt_isCorrect = $_POST['opt_isCorrect'];
 
 	// validate user input
 
 	$valid = true;
-	/* // gpcorser: never let user choose id
+	/*
 	if (empty($ques_id)) {
 		$ques_idError = 'Please enter Question ID';
 		$valid = false;
 	}
 	*/
-
 	if (empty($opt_text)) {
 		$opt_textError = 'Please enter Question Text';
 		$valid = false;
@@ -44,17 +40,15 @@ if ( !empty($_POST)) { // if not first time through
 	if ($valid) {
 		$pdo = Database::connect();
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "INSERT INTO qm_options (opt_text, opt_isCorrect) values(?, ?)"; // gpcorser: never let user choose id
+		$sql = "INSERT INTO qm_options (ques_id, opt_text, opt_isCorrect) values(?, ?, ?)"; 
 		$q = $pdo->prepare($sql);
-		$q->execute(array($opt_text,$opt_isCorrect));
+		$q->execute(array($ques_id, $opt_text,$opt_isCorrect));
 		Database::disconnect();
 
 		header("Location: qm_option_list.php");
 
 	}
 }
-
-include '/home/gpcorser/public_html/database/header.php'; //html <head> section
 
 ?>
 
@@ -68,7 +62,6 @@ include '/home/gpcorser/public_html/database/header.php'; //html <head> section
 			</div>
 
 			<form class="form-horizontal" action="qm_option_create.php" method="post">
-
 
 				<div class="control-group <?php echo !empty($ques_idError)?'error':'';?>">
 					<label class="control-label">Question</label>
@@ -85,7 +78,6 @@ include '/home/gpcorser/public_html/database/header.php'; //html <head> section
 						</select>
 					</div>
 				</div>
-
 
 				<div class="control-group <?php echo !empty($opt_textError)?'error':'';?>">
 					<br>
