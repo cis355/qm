ec11a01dbfe4aafc81ba0cda5bad09386fa74d02
deepakNeupane <dneupan1@csis.svsu.edu>
2018-03-19 11:20:10 -0400

    dneupan1: session.php included

--- qm_per_read.php
+++ qm_per_read.php
@@ -67,10 +67,10 @@
                         <label class="control-label">First Name: </label> <?php echo $data['fname'];?> <?php echo "</br>";?>
                         
                       <div class="control-group">
-                        <label class="control-label">Last Name: </label> <?php echo $data['lname'];?> <?php echo "</br>";?>
+                        <label class="control-label">Last Name : </label> <?php echo $data['lname'];?> <?php echo "</br>";?>
                       </div>
                       <div class="control-group">
-                        <label class="control-label">Email : </label> <?php echo $data['email'];?> <?php echo "</br>";?>
+                        <label class="control-label">Email     : </label> <?php echo $data['email'];?> <?php echo "</br>";?>
                       </div>
                         <div class="form-actions">
 						                <a class="btn" href="qm_per_list.php">Back</a>
