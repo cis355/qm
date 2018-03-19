74ba870704dbb40f68bea4c4305cc31e3dd297b8
Ryan A. Ott <raott@csis.svsu.edu>
2018-03-19 11:59:36 -0400

    "raott: updating qm_qa_d

--- qm_qa_delete.php
+++ qm_qa_delete.php
@@ -24,7 +24,7 @@ if ( !empty($_POST)) { // if user clicks "yes" (sure to delete), delete record
 else { // otherwise, pre-populate fields to show data to be deleted
 	$pdo = Database::connect();
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = "SELECT * FROM qm_attempts where id = ?";
+	$sql = "SELECT * FROM qm_attempts where quiz_id = ?";
 	$q = $pdo->prepare($sql);
 	$q->execute(array($id));
 	$data = $q->fetch(PDO::FETCH_ASSOC);
@@ -121,7 +121,7 @@ else { // otherwise, pre-populate fields to show data to be deleted
 			</div>
 				
 		</div>  <!-- end div: class="form-horizontal" -->
-
+		<p>Made by: Ryan Ott raott@svsu.edu</p>
     </div> <!-- end div: class="container" -->
 	
 </body>
