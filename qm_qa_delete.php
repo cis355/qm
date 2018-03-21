16f23a3d6bec265cb4fced93fc125cd4ff152a76
Ryan A. Ott <raott@csis.svsu.edu>
2018-03-21 12:20:59 -0400

    raott: updating qm_qa_delete.php

--- qm_qa_delete.php
+++ qm_qa_delete.php
@@ -8,7 +8,8 @@
 */
 include '/home/gpcorser/public_html/database/header.php'; // html <head> section
 require '/home/gpcorser/public_html/database/database.php';
-$id = $_GET['quiz_id'];
+$id = $_GET['attempt_id'];
+$per_id = $_GET['per_id'];
 if ( !empty($_POST)) { // if user clicks "yes" (sure to delete), delete record
 	$id = $_POST['id'];
 	
@@ -18,13 +19,13 @@ if ( !empty($_POST)) { // if user clicks "yes" (sure to delete), delete record
 	$q = $pdo->prepare($sql);
 	$q->execute(array($id));
 	Database::disconnect();
-	header("Location: qm_qa_list.php");
+	header("Location: qm_qa_list.php?per_id=$per_id");
 	
 } 
 else { // otherwise, pre-populate fields to show data to be deleted
 	$pdo = Database::connect();
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = "SELECT * FROM qm_attempts where quiz_id = ?";
+	$sql = "SELECT * FROM qm_attempts where id = ?";
 	$q = $pdo->prepare($sql);
 	$q->execute(array($id));
 	$data = $q->fetch(PDO::FETCH_ASSOC);
@@ -52,12 +53,12 @@ else { // otherwise, pre-populate fields to show data to be deleted
 			<!--<h2>&nbsp&nbsp<img src="ohno.jpg" alt="Oh No Dude"/></h2>-->
 		</div>
 		
-		<form class="form-horizontal" action="qm_qa_delete.php" method="post">
+		<form class="form-horizontal" action="qm_qa_delete.php?per_id=<?php echo $per_id;?>" method="post">
 			<input type="hidden" name="id" value="<?php echo $id;?>"/>
 			<p class="alert alert-error">Are you sure you want to delete?</p>
 			<div class="form-actions">
 				<button type="submit" class="btn btn-danger">Yes</button>
-				<a class="btn" href="qm_qa_list.php?per_id=$per_id">No</a>
+				<a class="btn" href="qm_qa_list.php?per_id=<?php echo $per_id;?>">No</a>
 			</div>
 		</form>
 		
