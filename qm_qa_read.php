7d4b08e57e97d4e9eb59f37ebeeaf441f2691526
Brandon Gage <bgage@csis.svsu.edu>
2018-03-26 10:42:26 -0400

    bgage: Updated qm_qa_list.php and created qm_qa_read.php

---
+++ qm_qa_read.php
@@ -0,0 +1,9 @@
+<?php
+include '/home/gpcorser/public_html/database/header.php'; // html <head> section
+require '/home/gpcorser/public_html/database/database.php';
+ ?>
+ <body style="background-color: lightblue !important";>
+     <div class="container">
+       <h3>Attempt</h3>
+     </div>
+   </body>
