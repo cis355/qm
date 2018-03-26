57546cab2c6b53f9bf01cb0d83d5a233f36e1148
Brandon Gage <bgage@csis.svsu.edu>
2018-03-26 11:35:28 -0400

    bgage: updated

--- qm_qa_read.php
+++ qm_qa_read.php
@@ -5,5 +5,10 @@ require '/home/gpcorser/public_html/database/database.php';
  <body style="background-color: lightblue !important";>
      <div class="container">
        <h3>Attempt</h3>
+       <?php
+          $pdo = Database::connect();
+          $attempt_id = $_GET['attempt_id'];
+          
+       ?>
      </div>
    </body>
